eddd29b4796dd3e4bb08feab805134a4fa111fd9
rubanRU <34646101+rubanRU@users.noreply.github.com>
2018-05-23 18:44:18 +0530

    Create mull.java

---
+++ mull.java
@@ -0,0 +1,39 @@
+import java.util.*;
+class subarr{
+     
+     public static void main(String aa[]){
+         int n;
+         Scanner s=new Scanner(System.in);
+         n=s.nextInt();
+         String y="";
+         String o="";
+         int z=0;
+         int u=1;
+         ArrayList<Integer>x=new ArrayList<Integer>();
+         int a[]=new int[n];
+         for(int i=0;i<n;i++){
+             a[i]=s.nextInt();
+              o=String.valueOf(a[i]);
+             y=y+o;
+         }
+        
+         for(int i=0;i<y.length();i++){
+             for(int j=i+1;j<y.length();j++){
+             String d=y.substring(i,j);
+             char ss[]=d.toCharArray();
+             
+             for(int k=0;k<ss.length;k++){
+                 int zz=Character.getNumericValue(ss[k]);
+                 u=u*zz;
+                 
+             }
+             
+              x.add(u);
+              u=0;
+             ss=null;
+             }
+         }
+         System.out.println(Collections.max(x));
+        
+         }
+     }
