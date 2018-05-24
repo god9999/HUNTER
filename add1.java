b7a567431f4f0f7b148c58034db9b716bb31017e
rubanRU <34646101+rubanRU@users.noreply.github.com>
2018-05-24 19:18:35 +0530

    Rename add.java to add1.java

---
+++ add1.java
@@ -0,0 +1,23 @@
+
+import java.util.*;
+class a{
+    public static void main(String aa[]){
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        String h=String.valueOf(n);
+        int u=0;
+        for(int i=h.length()-1;i>=0;i--){
+            int y=Character.getNumericValue(h.charAt(i));
+            u=u+y;
+            
+        }
+        String k=String.valueOf(u);
+        String jj=new StringBuffer(k).reverse().toString();
+        if(k.equals(jj)){
+            System.out.println("yes");
+        }
+        else{
+            System.out.println("no");
+        }
+    }
+}
