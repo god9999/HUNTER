b6f79d9260132ba7922982ff94ab70d906a5e91a
rubanRU <34646101+rubanRU@users.noreply.github.com>
2018-05-25 18:32:34 +0530

    Create ind.java

---
+++ ind.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class ind{
+    public static void main(String a[]){
+        Scanner s=new Scanner(System.in);
+        String j=s.next();
+        String k=s.next();
+        int y=j.indexOf(k);
+        System.out.println(y);
+    }
+}
