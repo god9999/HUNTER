1bce208f792091797900ddeb7168dd48fe45e872
rubanRU <34646101+rubanRU@users.noreply.github.com>
2018-05-27 17:52:54 +0530

    Create dele.java

---
+++ dele.java
@@ -0,0 +1,33 @@
+import java.util.*;
+class onec{
+    public static void main(String aa[]){
+        Scanner s=new Scanner(System.in);
+        String h=s.next();
+        String j="";
+        String j1="";
+        char v[]=h.toCharArray();
+        int y=h.length()/2;
+        for(int i=0;i<y;i++){
+            j=j+v[i];
+        }
+        if(h.length()%2==0){
+        for(int i=y;i<h.length();i++){
+            j1=j1+v[i];
+        }
+        }
+        else{
+          for(int i=y+1;i<h.length();i++){
+            j1=j1+v[i];
+        }  
+        }
+       String hh=j+j1;
+       String d=new StringBuffer(hh).reverse().toString();
+       
+        if(hh.equals(d){
+            System.out.println("yes");
+        }
+        else{
+            System.out.println("no");
+        }
+    }
+}
